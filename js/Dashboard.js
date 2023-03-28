b91bdf9fd948667aaf8e7a81e250e2338278ba14
firas98alashhab <127398270+firas98alashhab@users.noreply.github.com>
2023-03-28 12:43:01 +0300

    update

--- js/Dashboard.js
+++ js/Dashboard.js
@@ -3,6 +3,9 @@ const sidebar = body.querySelector(".sidebar");
 const toggle = body.querySelector(".toggle");
 const searchBtn = body.querySelector(".search-box");
 const headerTitle = body.querySelector(".title");
+let subMenu = document.getElementById("subMenu");
+const menuSideBar = body.querySelector(".my-menu-bar");
+
 
 toggle.addEventListener("click", () => {
   sidebar.classList.toggle("close");
@@ -13,8 +16,17 @@ toggle.addEventListener("click", () => {
 });
 
 
-let subMenu = document.getElementById("subMenu");
+function toggleMenu(){
+  subMenu.classList.toggle("open-menu");
+}
 
-    function toggleMenu(){
-      subMenu.classList.toggle("open-menu");
-    }
+function sideBarClose(){
+  sidebar.style.width = "0px";
+  sidebar.style.overflow = "hidden";
+  sidebar.style.opacity = "0";
+}
+
+function sideBarOpen(){
+  sidebar.style.width = "250px";
+  sidebar.style.opacity = "1";
+}
